66372819b813343bdc8b99e8f7e9fff8e3dff099
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2021-05-26 18:55:31 +0200

    Added animation to all the links

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const menuBtn = document.querySelector('.menu-btn');
-var menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
-});
+const animations = () => {
+
+    // DEFINIG ALL THE CLASSES
+    const menuBtn = document.querySelector('.menu-btn');
+    const burgerTwo = document.querySelector('.menu-btn_burger_2');
+    const burgerThree = document.querySelector('.menu-btn_burger_3');
+    const burgerOne = document.querySelector('.menu-btn_burger');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const list = document.querySelector('.nav-links');
+    
+    // EVENT ON CLICK
+    menuBtn.addEventListener('click', () => {
+        // TOGGLES THE ACTIVE CLASS FOR BURGER MENU
+        burgerTwo.classList.toggle('menu-btn_burger_2-active');
+        burgerThree.classList.toggle('menu-btn_burger_3-active');
+        burgerOne.classList.toggle('menu-btn_burger-active');
+        list.classList.toggle('nav-active');
+        
+        // FOR EVERY SINGLE <li> ELEMENT
+        navLinks.forEach((link, index) => {
+            // IF THERE IS NO ANIMATION 
+            // ANIMATE ALL THE LINKS
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinksFade .5s ease forwards ${index / 7 + 0.3}s`
+            }
+        });
+    });
+}
+
+animations();
